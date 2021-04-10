87c544ab87261ed10c5b7ba19f041fe22e8cc9e4
utkarsh2yadav <65600418+utkarsh2yadav@users.noreply.github.com>
2021-04-10 14:30:55 +0530

    shopkeeper interface added
    done

--- shopkeeper_home_page.sh
+++ shopkeeper_home_page.sh
@@ -1,1 +1,49 @@
 echo " Shopkeeper Home Page " 
+echo "                                             "
+echo "Press Following Keys for Following Operations"
+echo "                                             "
+echo "1)	View pending Orders"
+echo "2)    Mark for pickup"
+echo "											   "
+
+read operation_number
+
+if [ $operation_number -eq 1 ]
+then
+   echo "                                         "
+   echo "Enter Your shop Id"
+   echo "                                             "
+fi
+if [ $operation_number -eq 1 ]
+then
+    if [ $shop_id -eq 1 ]
+	then
+	   echo "                                             "
+	   echo "Enter Order Id"
+	   echo "                                             "
+
+	   read search_order
+
+	   echo "                                             "
+	   echo " *****************  Pending Orders *******************"
+	   grep  $search_order orders.txt
+	   echo "                                             "
+	elif [ $shop_id -eq 2 ]
+	then
+	   echo "                                             "
+	   echo "Enter Order Id"
+	   echo "                                             "
+
+	   read search_order
+
+	   echo "                                             "
+	   echo " *****************  Pending Orders *******************"
+	   grep  $search_order orders.txt
+	   echo "                                             "
+	fi
+elif [ $operation_number -eq 2 ]
+then
+	echo "                                              "
+	python item_dispatch.py
+	echo "	                                            "
+fi
